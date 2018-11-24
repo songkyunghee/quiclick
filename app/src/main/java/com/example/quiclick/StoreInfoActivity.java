c1c2be6a065906e6ad47bd573014244c20483292
songkyunghee <31922462+songkyunghee@users.noreply.github.com>
2018-11-24 16:24:03 +0900

    store 상세 액티비티 레이아웃 추가
    레이아웃 틀만 구현

---
+++ app/src/main/java/com/example/quiclick/StoreInfoActivity.java
@@ -0,0 +1,13 @@
+package com.example.quiclick;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class StoreInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_storeinfo);
+    }
+}
