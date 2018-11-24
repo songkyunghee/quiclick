11b16739ecd6dee5ff3b0c1ce053a4a2b96a3301
songkyunghee <31922462+songkyunghee@users.noreply.github.com>
2018-11-24 16:12:38 +0900

    splash 추가
    첫 화면에 click logo 뜨는 splashactivity 추가

---
+++ app/src/main/java/com/example/quiclick/SplashActivity.java
@@ -0,0 +1,22 @@
+package com.example.quiclick;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        try{
+            Thread.sleep(4000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+        finish();
+    }
+}
